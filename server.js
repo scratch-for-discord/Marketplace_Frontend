4d3d3fa47b567ae68bdbc21c778a3e91d4ff8b95
Retro <70205403+retrouser955@users.noreply.github.com>
2022-05-07 18:27:32 +0630

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const packages = require('./packages.json')
+const app = express()
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    res.render('homepage/index', {
+        packageArray: packages.extensions
+    })
+})
+app.get('/:extensions', (req, res) => {
+    
+})
+app.listen(3000, () => console.log('app is ready!'))
