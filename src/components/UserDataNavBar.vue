96f7d2ba62d1a59bedc540d0629b082eecf149d1
mrredo <mrredogt@gmail.com>
2022-10-30 10:24:39 +0200

    Update UserDataNavBar.vue

--- src/components/UserDataNavBar.vue
+++ src/components/UserDataNavBar.vue
@@ -25,26 +25,26 @@ const comp = defineComponent({
             })
             this.user = await response.data;
         } catch (error) {
-            console.log((error as AxiosError).isAxiosError)
+            console.log((error as AxiosError))
         }
-        const options = {
-  method: 'GET',
-credentials: "include"
-};
+//         const options = {
+//   method: 'GET',
+// credentials: "include"
+// };
 
-fetch('http://localhost:3020/api/user/', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+// fetch('http://localhost:3020/api/user/', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
     
-    // const data = await response;
-        console.log(response?.status)
-    }
+//     // const data = await response;
+//         console.log(response?.status)
+//     }
     //   mounted () {
 //     axios
 //       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
 //       .then(response => (this.data = response))
-//   }
+  }
 })
 export default comp
 
